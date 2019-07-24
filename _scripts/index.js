33179186690d4e656382f00f4e3a6af1e5fcc203
Kyle Kashuba <kyle@kashubadev.com>
2019-07-24 00:49:56 -0700

    Prevent all initial transitions

--- _scripts/index.js
+++ _scripts/index.js
@@ -1,21 +1,5 @@
 import '../_styles/index.sass';
 
-let currentIndex = 0;
-const slides = Array.from(document.querySelectorAll('.marquee .slide'));
-
-slides[0].setAttribute('data-active', true);
-
-setInterval(() => {
-  const previousIndex = currentIndex;
-  currentIndex++;
-
-  if (currentIndex === slides.length) {
-    currentIndex = 0;
-  }
-
-  slides[previousIndex].setAttribute('data-active', false);
-
-  setTimeout(() => {
-    slides[currentIndex].setAttribute('data-active', true);
-  }, 1000);
-}, 5000);
+setTimeout(() => {
+  document.body.className = "";
+}, 100)
